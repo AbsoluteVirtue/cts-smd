b27c9584239c06c76e74eb296d36664e0f427107
AbsoluteVirtue <3hitemouse@gmail.com>
2025-04-28 00:07:25 +0300

    Update index.js

--- konva/scripts/index.js
+++ konva/scripts/index.js
@@ -11,6 +11,30 @@ function getRandomFloat(min, max) {
 	return Math.random() * (max - min) + min;
 }
 
+function writeMessage(message) {
+	text.text(message);
+}
+
+function isPointOnLine(px, py, x1, y1, x2, y2, width) {
+	return distancePointFromLine(px, py, x1, y1, x2, y2, width) <= width / 2;
+}
+
+function distancePointFromLine(x0, y0, x1, y1, x2, y2) {
+	return Math.abs((x2 - x1) * (y1 - y0) - (x1 - x0) * (y2 - y1)) / Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+}
+
+function distance(x1, x2, y1, y2) {
+	var a = Math.abs(x1 - x2);
+	var b = Math.abs(y1 - y2);
+	var c = Math.sqrt((a * a) + (b * b));
+	return c;
+}
+
+// hypo = longer     + 3/7 * shorter
+// hypo = longer / 2 + 7/8 * shorter
+
+/////////////////////////////////////////////////////
+
 var stage = new Konva.Stage({
 	container: 'container', // id of container <div>
 	width: window.innerWidth,
@@ -28,29 +52,36 @@ const text = new Konva.Text({
 	fill: 'black',
 });
 
-function writeMessage(message) {
-	text.text(message);
-}
+const canvas = document.createElement('canvas');
+canvas.width = 200;
+canvas.height = 150;
+const ctx = canvas.getContext('2d');
 
 const grid = new Array();
 
-// const xstart = stage.width() / 2;
-// const ystart = stage.height() / 2;
+// const xcenter = stage.width() / 2;
+// const ycenter = stage.height() / 2;
+
 const xstart = 100;
 const ystart = 100;
 const rad = 70;
 const h = (rad * 1.732) / 2;
 
-// hypo = longer     + 3/7 * shorter
-// hypo = longer / 2 + 7/8 * shorter
+// layer.add(new Konva.Circle({
+// 	x: stage.width() / 2 + 1.5 * rad,
+// 	y: stage.height() / 2 - 4.1 * rad,
+// 	radius: rad,
+// 	fill: 'black',
+// 	draggable: true,
+// }));
 
-const circle = new Konva.Circle({
-	x: stage.width() / 2,
-	y: stage.height() / 2,
-	radius: 70,
-	fill: 'black',
-	draggable: true,
-});
+// layer.add(new Konva.Circle({
+// 	x: stage.width() / 2,
+// 	y: stage.height() / 2,
+// 	radius: rad,
+// 	fill: 'black',
+// 	draggable: true,
+// }));
 
 for (let row = 0; row < 10; row++) {
 
@@ -146,42 +177,70 @@ for (let row = 0; row < 10; row++) {
 			edg: edges,
 		});
 
-		// hex.on('dblclick', () => {
-		//   hex.fill('red');
-		//   hex.stroke('red');
-		//   edges.forEach((item) => {
-		//     item.stroke('red');
-		//   });
-		// });
+		hex.on('dblclick', () => {
+			hex.fill('red');
+			hex.stroke('red');
+			edges.forEach((item) => {
+				item.stroke('red');
+			});
+		});
 
 		grid.push(hex);
 	}
-
 }
 
 let path = new Konva.Line({
-	points: [],
+	points: [
+
+	],
 	stroke: 'green',
 	strokeWidth: 7,
+	lineCap: 'round',
+	lineJoin: 'round',
+	// bezier: true,
 });
 
-function isPointOnLine(px, py, x1, y1, x2, y2, width) {
-	return distancePointFromLine(px, py, x1, y1, x2, y2, width) <= width / 2;
-}
+let traj = new Konva.Line({
+	points: [
+		281.86 - 3 * h,
+		310,
+		1069.92,
+		520
+	],
+	stroke: 'black',
+	strokeWidth: 4,
+});
 
-function distancePointFromLine(x0, y0, x1, y1, x2, y2) {
-	return Math.abs((x2 - x1) * (y1 - y0) - (x1 - x0) * (y2 - y1)) / Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
-}
+const startc = new Konva.Circle({
+	x: 281.86 - 3 * h,
+	y: 310,
+	radius: 20,
+	fill: 'black',
+	draggable: true,
+});
 
-function distance(x1, x2, y1, y2) {
-	var a = Math.abs(x1 - x2);
-	var b = Math.abs(y1 - y2);
-	var c = Math.sqrt((a * a) + (b * b));
-	return c;
-}
+const endc1 = new Konva.Circle({
+	x: 1069.92,
+	y: 520,
+	radius: 20,
+	fill: 'white',
+	draggable: true,
+});
+
+const endc2 = new Konva.Circle({
+	x: 1069.92,
+	y: 520,
+	radius: 30,
+	fill: 'black',
+	draggable: true,
+});
+
+layer.add(startc);
+layer.add(endc2);
+layer.add(endc1);
 
 grid.forEach((item) => {
-	item.on('click', () => {
+	item.on('wheel', () => {
 		let pos = stage.getPointerPosition();
 		let points = item.getAttr('edg')[0].points();
 
@@ -240,6 +299,27 @@ grid.forEach((item) => {
 	layer.add(item);
 });
 
+// layer.add(traj);
+
+// layer.add(
+// 	new Konva.Rect({
+// 		x: stage.width() / 2,
+// 		y: 210,
+// 		width: 100,
+// 		height: 100,
+// 		fill: 'black',
+// 		strokeWidth: 4
+// 	})
+// );
+
 layer.add(path);
 
 stage.add(layer);
+
+// let angle1 = 100;
+// let angle2 = 120;
+// var cs = Math.cos(angle1), sn = Math.sin(angle1);
+// var he = Math.cos(angle2);
+// var ak = 100 * cs, bk = -100 * sn, ck = 200;
+// var dk = h * 100 * sn, ek = h * 100 * cs, fk = 200;
+// ctx.setTransform(ak, dk, bk, ek, ck, fk);
